7180da16e3e839915b04ee93dc3c5004117507bc
Rahul Karki <karkirahul1409@gmail.com>
2025-05-17 14:04:30 +0545

    commit for tut2.py

--- tut2.py
+++ tut2.py
@@ -1,20 +1,22 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template, request , redirect, url_for
+
 app = Flask(__name__)
 
-@app.route('/admin')
-def hello_admin():
-   return 'Hello Admin'
+@app.route("/")
+def index():
+    return render_template("form.html")
 
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-   return 'Hello %s as Guest' % guest
+@app.route("/away", methods=["GET", "POST"])
+def home():
+    name = ""
+    if request.method == "POST":
+        name = request.form.get("name")
+    return redirect(url_for("display",name = name))
 
-@app.route('/user/<name>')
-def hello_user(name):
-   if name =='admin':
-      return redirect(url_for('hello_admin'))
-   else:
-      return redirect(url_for('hello_guest',guest = name))
+@app.route("/display<name>")
+def display(name):
+    return f" hey whats up {name}"
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True, port=5000)
+
